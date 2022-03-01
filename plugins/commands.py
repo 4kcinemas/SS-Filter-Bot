a29f30f54428a955b4dd4df202f38828d0906f0b
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-03-01 07:50:24 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("🔰 Our Channel 🔰", url="https://t.me/SS_Linkz"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("❗ Movie Group 1 ❗", url="https://t.me/NetflixMovies_sslinkz"),
+                    InlineKeyboardButton("❕ Movie Group 2 ❕", url="https://t.me/ssfilmsgroup")
                 ]
             ]
         ),
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://t.me/ss_linkz")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
