50ce6179119edfdce0016a297a031b441e4779d3
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-03-01 07:42:43 +0530

    Update script.py

--- script.py
+++ script.py
@@ -48,13 +48,13 @@ See <i>/help</i> for commands and more details.</b>
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 
-<b>© @TroJanzHEX</b>
+<b>© @SS_Linkz</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : SS Filter Bot</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creater :</b> @Sarathi_admin    
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
